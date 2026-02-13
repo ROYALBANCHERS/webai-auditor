25edf25163208cf1e8e5c1c2eb1302a22681547c
rishabh023 <growthing868@gmail.com>
2026-02-13 14:53:11 +0530

    fix: add --disable-dev-shm-usage flag for Render compatibility

--- backend/realUserAuditor.js
+++ backend/realUserAuditor.js
@@ -2976,7 +2976,7 @@ export const auditWebsiteWithBrowser = async (inputUrl) => {
 
     browser = await chromium.launch({
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
     });
 
     const context = await browser.newContext({
